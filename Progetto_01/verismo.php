035c2c32251a91901773eb12a8f41eaabb89d2e2
Cris Flores <125544276+Reyven30@users.noreply.github.com>
2025-10-28 09:45:40 +0100

    Add files via upload

---
+++ Progetto_01/verismo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="it">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Il Verismo</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <body>
+
+        <div class="text">
+        <h1>Il Verismo</h1>
+        </div>
+        
+    </body>
+</html>
